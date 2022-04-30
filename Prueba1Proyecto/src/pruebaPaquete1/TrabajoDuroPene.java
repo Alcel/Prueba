bb744ad4c4a9b54e3c66e4c48286ebaa35360520
Nicolás <Nicols@DESKTOP-3C50H6B>
2022-04-30 13:46:29 +0200

    Soy muy gracioso

---
+++ Prueba1Proyecto/src/pruebaPaquete1/TrabajoDuroPene.java
@@ -0,0 +1,8 @@
+package pruebaPaquete1;
+
+public class TrabajoDuroPene {
+	public static void main(String[] args) {
+		System.out.println("no");
+	}
+
+}
---
+++ Prueba1Proyecto/src/tret/rt5ery.java
@@ -0,0 +1,7 @@
+package tret;
+
+public class rt5ery {
+	private void name() {
+		System.out.println("put");
+	}
+}
